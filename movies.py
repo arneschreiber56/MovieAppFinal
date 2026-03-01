9dd4ea7012e6ba5b0e2ac1ca2d9e3b0263d673c0
Arne Schreiber <arneschreiber@gmx.de>
2026-03-01 21:33:54 +0100

    Database changed from Dictionary to List of Dictionaries. Year as Key for Value Year of Release included. Code adapted for new data structure

--- movies.py
+++ movies.py
@@ -75,29 +75,33 @@ def start_screen():
 
 def movie_db_function_list(movies):
     """
-    Gibt die Filme in dem Film-DB-Dictionary aus
-    :param movies: Film-DB-Dictionary
+    Gibt die Filme in der Film-DB-Liste aus
+    :param movies: Film-DB-Liste
     :return: None
     """
     print(f"\n{len(movies)} movies in total")
-    for movie, rating in movies.items():
-        print(movie + ': ' + str(rating))
+    for movie in movies:
+        print(f"{movie['title']}: {movie['rating']} ({movie['year']})")
     console.input("\n[dim]Press enter to continue[/dim]")
     return
 
 
 def movie_db_function_add(movies):
     """
-    Ergänzt einen neuen Film in der Film-DB-Dictionary. User gibt Filmtitel
-    und Rating an.
-    :param movies: Film-DB-Dictionary
-    :return: aktualisiertes Film-DB-Dictionary
+    Ergänzt einen neuen Film in der Film-DB-Liste. User gibt Filmtitel, Rating
+    und Erscheinungsjahr an.
+    :param movies: Film-DB-Liste
+    :return: aktualisierte Film-DB-Liste
     """
     new_movie_name = input("\nEnter new movie name: ")
     new_movie_rating_float = float(input("Enter new movie rating(0-10): "))
+    new_movie_year = int(input("Enter release year: "))
     if 1 <= new_movie_rating_float <= 10:
-        movies[new_movie_name] = new_movie_rating_float
-        print(f"Movie {new_movie_name} successfully added")
+        movies.append({
+            "title": new_movie_name,
+            "rating": new_movie_rating_float,
+            "year": new_movie_year
+        })
         console.input("\n[dim]Press enter to continue[/dim]")
         return movies
     else:
@@ -114,40 +118,37 @@ def movie_db_function_del(movies):
     :return: Aktualisiertes Film-DB-Dictionary
     """
     movie_to_del = input("\nEnter movie name to delete: ")
-    if movie_to_del in movies:
-        del movies[movie_to_del]
-        print(f"Movie {movie_to_del} successfully deleted")
-        console.input("\n[dim]Press enter to continue[/dim]")
-        return movies
+    for movie in movies:
+        if movie["title"] == movie_to_del:
+            movies.remove(movie)
+            print(f"Movie {movie_to_del} successfully deleted")
+            return movies
     else:
         print(f"Movie {movie_to_del} doesn't exist!")
-        console.input("\n[dim]Press enter to continue[/dim]")
         return movies
 
 
 def movie_db_function_update(movies):
     """
-    Aktualisiert einen Filmeintrag in dem Film-DB-Dictionary. User muss
+    Aktualisiert einen Filmeintrag in der Film-DB-Liste. User muss
     Filmtitel korrekt angeben und dann auf Nachfrage das aktualisierte Rating
     eingeben.
-    :param movies: Film-DB-Dictionary
-    :return: Aktualisiertes Film-DB-Dictionary
+    :param movies: Film-DB-Liste
+    :return: Aktualisierte Film-DB-Liste
     """
     movie_to_update = input("\nEnter movie name: ")
-    if movie_to_update in movies:
-        new_rating = float(input("Enter new movie rating (1-10): "))
-        if 1 <= new_rating <= 10:
-            movies[movie_to_update] = new_rating
-            print(f"Movie {movie_to_update} successfully updated")
-            console.input("\n[dim]Press enter to continue[/dim]")
-            return movies
-        else:
-            print(f"Rating {new_rating} is invalid")
-            console.input("\n[dim]Press enter to continue[/dim]")
-            return movies
+    for movie in movies:
+        if movie["title"] == movie_to_update:
+            new_rating = float(input("Enter new movie rating (1-10): "))
+            if 1 <= new_rating <= 10:
+                movie["rating"] = new_rating
+                print(f"Movie {movie_to_update} successfully updated")
+                return movies
+            else:
+                print(f"Rating {new_rating} is invalid")
+                return movies
     else:
         print(f"Movie {movie_to_update} doesn't exist!")
-        console.input("\n[dim]Press enter to continue[/dim]")
         return movies
 
 
@@ -160,22 +161,20 @@ def movie_db_function_stats(movies):
     :param movies: Film-DB-Dictionary
     :return: None
     """
-    average_rating = statistics.mean(movies.values())
-    median_rating = statistics.median(movies.values())
-    print(f"Average rating: {average_rating}")
+    average_rating = statistics.mean(movie["rating"] for movie in movies)
+    median_rating = statistics.median(movie["rating"] for movie in movies)
+    print(f"Average rating: {average_rating:.2f}")
     print(f"Median rating: {median_rating}")
     sorted_to_ratings = sorted(
-      movies.items(),
-      key=lambda r: (r[1], r[0]),
-      reverse=True
+        movies,
+        key=lambda m: (m["rating"], m["title"]),
+        reverse=True
     )
     best_movie = sorted_to_ratings[0]
     worst_movie = sorted_to_ratings[-1]
-    for movie in sorted_to_ratings:
-        if movie[1] == best_movie[1]:
-            print(f"Best movie: {movie[0]}, {movie[1]}")
-        elif movie[1] == worst_movie[1]:
-            print(f"Worst movie: {movie[0]}, {movie[1]}")
+
+    print(f"Best movie: {best_movie['title']}, {best_movie['rating']}")
+    print(f"Worst movie: {worst_movie['title']}, {worst_movie['rating']}")
     console.input("\n[dim]Press enter to continue[/dim]")
     return
 
@@ -183,16 +182,14 @@ def movie_db_function_stats(movies):
 def movie_db_function_random(movies):
     """
     Die Funktion wählt einen zufälligen Film mithilfe der random library aus und
-    gibt diesen samt Rating aus.
-    :param movies: Film-DB-Dictionary
+    gibt diesen samt Rating und Erscheinungsjahr aus.
+    :param movies: Film-DB-Liste
     :return: None
     """
-    movie_list = list(movies.keys())
-    # Wandelt die Movie-Keys (type dictionary view object)in eine Liste um
-    random_movie = random.choice(movie_list)
-    random_rating = movies[random_movie]
+    random_movie = random.choice(movies)
     print(
-        f"\nYour movie for tonight: {random_movie}, it's rated {random_rating}"
+        f"\nYour movie for tonight: {random_movie['title']}, "
+        f"it's rated {random_movie['rating']} ({random_movie['year']})"
     )
     console.input("\n[dim]Press enter to continue[/dim]")
     return
@@ -204,18 +201,16 @@ def movie_db_function_search(movies):
     Datenbank, auch wenn der Titel nicht ganz korrekt eingegeben wird.
     Gibt exakten Treffer wieder. Wenn kein exakter Treffer gefunden wurde,
     wird mittels Fuzzy Matching ähnliche Treffer ausgegeben, sofern gefunden.
-    :param movies: Film-DB-Dictionary
+    :param movies: Film-DB-Liste
     :return: None
     """
     # zum Fuzzy Matching
     what_to_search = input("\nEnter part of movie name: ").lower()
-    movie_list = list(movies.keys())
     # normale Suche
     found_any = False
-    for i in range(len(movies)):
-        movie = movie_list[i]
-        if what_to_search in movie.lower():
-            print(f"{movie}, {movies[movie]}")
+    for movie in movies:
+        if what_to_search in movie["title"].lower():
+            print(f"{movie['title']}, {movie['rating']} ({movie['year']})")
             found_any = True
     if found_any:
         console.input("\n[dim]Press enter to continue[/dim]")
@@ -224,9 +219,11 @@ def movie_db_function_search(movies):
     print("\nMovie not found!")
     # Mit der get_close_matches-Funktion von difflib ähnliche Einträge
     # in movielist finden und als Liste ausgeben
+    # Fuzzy Matching vorbereiten mit einer Liste von Filmtiteln:
+    movie_titles = [movie["title"] for movie in movies]
     close_matches = difflib.get_close_matches(
         what_to_search,
-        movie_list,
+        movie_titles,
         # Anzahl der maximalen Ausgabe ähnlicher Filmnamen
         n=3,
         # Sensitivtät der Suche 0.1 → können total unterschiedlich sein,
@@ -237,7 +234,12 @@ def movie_db_function_search(movies):
         print("Similar movies found:")
         print(close_matches)
         for similar_name in close_matches:
-            print(f"  - {similar_name}, {movies[similar_name]}")
+            for movie in movies:
+                if movie["title"] == similar_name:
+                    print(f"  - {movie['title']}, "
+                          f"{movie['rating']} "
+                          f"({movie['year']})"
+                          )
     else:
         print("No similar movies found.")
     console.input("\n[dim]Press enter to continue[/dim]")
@@ -247,16 +249,15 @@ def movie_db_function_search(movies):
 def movie_db_function_sort(movies):
     """
     Sortiert Filme absteigend nach primär nach Rating und sekundär alphabetisch
-    :param movies: Film-DB-Dictionary
+    :param movies: Film-DB-Liste
     :return: None
     """
     sorted_to_ratings = sorted(
-        movies.items(), key=lambda r: (r[1], r[0]), reverse=True)
-    # dict() wandelt das dictionary view object wieder in das dann sortierte
-    # dictionary sorted_to_ratings um.
+        movies,
+        key=lambda m: (-m["rating"], m["title"])
+    )
     for movie in sorted_to_ratings:
-        print(f"{movie[0]}: {movie[1]}")
-
+        print(f"{movie['title']}: {movie['rating']} ({movie['year']})")
     console.input("\n[dim]Press enter to continue[/dim]")
     return
 
@@ -266,11 +267,11 @@ def movie_db_function_histo(movies):
     Erstellt mittels matplotlib.pyplot ein Histogramm der Filmratings.
     User wird nach Filename für Histogramm gefragt und das Histogramm wird unter
     dem Filename als .png-File im Working Directory gespeichert.
-    :param movies: Film-DB-Dictionary
+    :param movies: Film-DB-Liste
     :return: None
     """
     # Brauche hier eine Liste von allen Rankings
-    all_rankings_list = list(movies.values())
+    all_rankings_list = [movie["rating"] for movie in movies]
     # Erstellung Histogramm, skaliert automatisch den x-Achsenbereich,
     # erstellt 10 bins mit 1 Schrittweite
     set_binwidth = list(range(1, 10))
@@ -301,19 +302,19 @@ def movie_db_function_quit(_):
 
 def main():
     # Dictionary zum Speichern der Filmtitel und Ratings
-    movies = {
-        "The Shawshank Redemption": 9.5,
-        "Pulp Fiction": 8.8,
-        "The Room": 3.6,
-        "The Godfather": 9.2,
-        "The Godfather: Part II": 9.0,
-        "The Dark Knight": 9.0,
-        "12 Angry Men": 8.9,
-        "Everything Everywhere All At Once": 8.9,
-        "Forrest Gump": 8.8,
-        "Star Wars: Episode V": 8.7,
-
-    }
+    movies = [
+        {"title": "The Shawshank Redemption", "rating": 9.5, "year": 1994},
+        {"title": "Pulp Fiction", "rating": 8.8, "year": 1994},
+        {"title": "The Room", "rating": 3.6, "year": 2003},
+        {"title": "The Godfather", "rating": 9.2, "year": 1972},
+        {"title": "The Godfather: Part II", "rating": 9.0, "year": 1974},
+        {"title": "The Dark Knight", "rating": 9.0, "year": 2008},
+        {"title": "12 Angry Men", "rating": 8.9, "year": 1957},
+        {"title": "Everything Everywhere All At Once", "rating": 8.9,
+         "year": 2022},
+        {"title": "Forrest Gump", "rating": 8.8, "year": 1994},
+        {"title": "Star Wars: Episode V", "rating": 8.7, "year": 1980},
+    ]
     functions_dictionary = {
         "1": movie_db_function_list,
         "2": movie_db_function_add,
